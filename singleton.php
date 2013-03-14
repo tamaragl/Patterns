4d7b5bb73154107f7c896b0ea6a78f7c3e50802f
Tamara Garcia <tamaragl86@gmail.com>
2013-03-14 23:46:18 +0100

    Singleton example finished

--- singleton.php
+++ singleton.php
@@ -1,13 +1,16 @@
 <?php
-/**
+
+ /**
  * Singleton example
+ *----------------------------------------------------------------
+ * Este patrón tiene el propósito de asegurar que esta clase 
+ * solo tenga una ÚNICA INSTANCIA y proporciona un punto de
+ * acceso global a la misma. USEFULL: archivos de configuración.
  */
 
 class Singleton
-{
-	
+{	
 	private static $instance;
-
 	
 	private function __construct()
 	{
@@ -23,6 +26,39 @@ class Singleton
 		return self::$intance;
 	}
 
+	private __clone()
+	{
+		throw new Exception('No clone');
+	}
+}
+
+
+
+/**
+ * Singleton example commented
+ */
+class Singleton
+{	
+	//contenedor de la instancia. solo accesible desde la clase
+	private static $instance;
+	
+	//previene la creación de objetos via new
+	private function __construct()
+	{
+
+	}
+
+	//static para poder acceder a ella desde cualquier parte Singleton::getInstance()
+	public static function getInstance()
+	{
+		if( null === self::$intance )
+		{
+			self::$instance = new self();
+		}
+		return self::$intance;
+	}
+
+	//Deshabilitamos el uso de __clone
 	private __clone()
 	{
 		throw new Exception('No clone');
